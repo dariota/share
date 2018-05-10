bc1ef359a7714dc955605fe440023944adecdb09
Dário Tavares Antunes <dario@ntun.es>
2018-05-10 17:05:09 +0100

    Make share self-contained

--- lib/config.sh
+++
@@ -1,8 +0,0 @@
-config_read_file() {
-    (grep -E "^${2}=" -m 1 "${1}" 2>/dev/null || echo "VAR=''") | head -n 1 | cut -d '=' -f 2-;
-}
-
-config_get() {
-    val="$(config_read_file ~/.config/share/config "${1}")";
-    printf -- "%s" "${val}";
-}
